138f7d7af1b1635897f4b8c2fb4830f34a1a42d2
Christopher Stein <cstein@bmcc.cuny.edu>
2016-04-07 15:26:03 -0400

    Adding in php and styling to get the archive page to show in a more compact style

---
+++ template-parts/content-archive.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template part for displaying a list of posts in an archive
+ *
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	</header><!-- .entry-header -->
+    <?php twentysixteen_excerpt(); ?>
+    <?php twentysixteen_post_thumbnail(); ?>
+	<footer class="entry-footer">
+		<?php twentysixteen_entry_meta(); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-## -->
