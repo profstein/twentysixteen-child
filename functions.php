f546210c166735b542aa1a50e75602e3a066cbce
Christopher Stein <cstein@bmcc.cuny.edu>
2016-04-07 11:43:12 -0400

    adding functions.php and enqueing styles from parent theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * ADD IN PARENT STYLES
+ * Faster than @import
+ */
+add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
+function my_child_theme_scripts() {
+    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+}
+
+
+
+
+?>
